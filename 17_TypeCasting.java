bcae28e9ade769c94b32391945061332f8c65382
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-24 22:46:28 +0530

    Add files via upload

---
+++ 17_TypeCasting.java
@@ -0,0 +1,11 @@
+// Type Casting
+
+public class Example
+{
+    public static void main(String[] args) 
+    {
+        int n = (int)12.2;
+        int s = (int)Math.sqrt(25);
+        System.out.println(n + " " + s);
+    }
+}
