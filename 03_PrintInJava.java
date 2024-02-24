088374dc487a76e3a7057cdeab26712b511fd8f0
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-24 20:30:33 +0530

    Add files via upload

---
+++ 03_PrintInJava.java
@@ -0,0 +1,15 @@
+// println() is similar to print() but it adds a \n character after the text is displayed
+
+// print() method in Java is used to display a text on the console
+
+// printf() method allows formatted o/p. It is similar to printf() in C Language
+
+public class PrintInJava 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello All !");
+        System.out.print("Welcome to Java Programming");  
+        System.out.printf(" %d ",10);      
+    }    
+}
