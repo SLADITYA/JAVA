b5f1beac400e19761881695eada479dc4726f470
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-24 22:49:11 +0530

    Add files via upload

---
+++ 18_CharactersInJava.java
@@ -0,0 +1,15 @@
+// Characters in Java
+
+public class Example
+{
+    public static void main(String[] args) 
+    {
+        // Java supports unicode
+        char ch1 = 97;   // 'a'
+        char ch2 = 65;   // 'A'
+        char ch3 = 3077;    
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+    }
+}
