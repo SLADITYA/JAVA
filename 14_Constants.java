c28d00440fa15963c4b15a33839261e20f78e757
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-24 21:59:58 +0530

    Add files via upload

---
+++ 14_Constants.java
@@ -0,0 +1,14 @@
+// In java constants are defined using final keyword
+
+public class Example
+{
+    public static void main(String[] args) 
+    {
+        final double PI = 3.14;
+        // PI = 5.14;   shows some error as we are reassigning the value of the constant
+        // PI = 7.14;
+        System.out.println(PI);    
+    }    
+}
+
+// Naming Convection - We use UPPER_SNAKE_CASE to declare constants in Java
