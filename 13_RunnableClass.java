3a00eeede5ba685eaafcd8fe7cc5c031b08a2be0
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-24 21:49:03 +0530

    Add files via upload

---
+++ 13_RunnableClass.java
@@ -0,0 +1,25 @@
+// Byte Code
+
+// Java is platform independent i.e you can run an exe file (class file) directly in any OS
+
+// Runnable class - The classes which contain a main function
+
+class ABC{
+}
+
+class PQR{
+}
+
+class XYZ{
+	public static void main(String[] args)			// Runnable class
+	{
+		System.out.print("Hello, this is class XYZ");
+	}
+}
+
+class Aditya{						
+	public static void main(String[] args)			// Runnable class
+	{
+		System.out.print("Hello, this is class Aditya");
+	}
+}
