117cb7502fb606663f41a9f574ebc749ea8eff22
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2024-02-25 12:21:31 +0530

    Add files via upload

---
+++ 28_StringArrays.java
@@ -0,0 +1,13 @@
+// String Arrays
+
+public class Example 
+{
+    public static void main(String[] args) 
+    {
+        String[] arr = {"abc", "xyz"};
+        for (String eachString : arr) 
+        {
+            System.out.println(eachString);    
+        }
+    }
+}
